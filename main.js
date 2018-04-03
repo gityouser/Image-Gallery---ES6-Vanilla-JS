4c6ae5d3b513fdb52ced27a6e809ea31c7b4f294
gityouser <32172553+gityouser@users.noreply.github.com>
2018-04-03 14:16:08 +0300

    Added auto/prev/next buttons.

--- main.js
+++ main.js
@@ -1,35 +1,45 @@
 const current = document.querySelector("#current");
 const imgs = document.querySelectorAll(".imgs img");
 const auto = document.querySelector(".auto");
+const backwardsButton = document.querySelector('.backwards');
+const forwardsButton = document.querySelector('.forwards');
+
 const opacity = 0.6;
 let currentIndex = 0;
 let interval;
 
 function setImage(index) {
-    const image = imgs[index];
-    imgs.forEach((img) => img.style.opacity = 1);
-    image.style.opacity = opacity;
-    current.classList.add("fade-in");
+  const image = imgs[index];
     current.src = image.src;
-    setTimeout(() => current.classList.remove("fade-in"), 500);
+    styleImgs(index);
+}
+
+function styleImgs(index) {
+  const image = imgs[index];
+  imgs.forEach((img) => img.style.opacity = 1);
+  image.style.opacity = opacity;
+  current.classList.add("fade-in");
+  setTimeout(() => current.classList.remove("fade-in"), 500);
 }
 
 function loop() {
-  currentIndex = currentIndex < imgs.length -1 ? currentIndex + 1: 0
+  currentIndex = currentIndex < imgs.length -1 ? currentIndex + 1 : 0
   setImage(currentIndex);
   console.log(currentIndex);
 }
 
-setImage(currentIndex);
+backwardsButton.addEventListener('click', () => currentIndex > 0 ? (currentIndex -= 1, setImage(currentIndex)) : currentIndex = 0);
+forwardsButton.addEventListener('click', () => currentIndex < imgs.length - 1? (currentIndex += 1, setImage(currentIndex)) : currentIndex = 0);
+
 imgs.forEach((e, index) => e.addEventListener('click', () => setImage(index)));
-interval = setInterval(loop, 1000);
+interval = setInterval(loop, 2000);
 
 auto.addEventListener('click', () => {
   if(interval) {
     clearInterval(interval);
     interval = null;
   } else {
-    interval = setInterval(loop, 1000);
+    interval = setInterval(loop, 2000);
   }
 })
 
